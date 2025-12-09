21d92391cdd8132566a4d70cdbbeeda3530cdc76
Bagharia <hu.alexis@hotmail.com>
2025-12-09 01:23:12 +0100

    test endpoint

---
+++ backend/public/index.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// CORS headers
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+use Presentation\Api\Router;
+use Presentation\Api\Controllers\AuthApiController;
+use Presentation\Api\Controllers\UserApiController;
+use Presentation\Api\Controllers\OwnerApiController;
+
+// Instancier les contrôleurs
+$authController = new AuthApiController();
+$userController = new UserApiController();
+$ownerController = new OwnerApiController();
+
+// Instancier le router
+$router = new Router($authController, $userController, $ownerController);
+
+// Dispatcher la requête
+$router->dispatch();
